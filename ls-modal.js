f157610551103cdc17b949ed400413ecc84cbaea
Lukas Sawon <lukaszsawon@gmail.com>
2021-07-11 23:34:36 +0200

    first

---
+++ ls-modal.js
@@ -0,0 +1,44 @@
+// initializing button
+const showModal = document.querySelector(".ls-modal");
+
+const callModal = (header, body, button) => {
+  const modal = document.createElement('div');
+  const modalHtml = `
+      <div class="ls-modal-background ls-modal-flex">
+          <div class="ls-modal-inner ls-modal-flex">
+              <div class="ls-modal-contents ls-modal-flex">
+                <span class="ls-modal-header">
+                    <b>${header || "Attention!"}</b>
+                </span>
+                <br>
+                <span class="ls-modal-body">
+                    ${body || "This is default modal body text"}
+                </span>
+            </div>
+            <div class="ls-modal-button-wrapper ls-modal-flex">
+              <button class="ls-modal-button">${button || "OK"}</button>
+            </div>
+          </div>
+      </div>`;  
+  
+  document.body.appendChild(modal).innerHTML = modalHtml;
+  
+  const modalWindow = document.querySelector(".ls-modal-background");
+  const modalButton = document.querySelector(".ls-modal-button");
+
+  const animateExit = (appliedTo) => {
+    setTimeout(() => document.body.removeChild(modal), 180);
+    (function() {
+      appliedTo.classList.add('fade');
+    }());
+  }
+
+  modalButton.addEventListener("click", async () => {
+    animateExit(modalWindow)
+  });
+  modalWindow.addEventListener("click", (e) => {
+    if (e.target === modalWindow) {
+      animateExit(modalWindow)
+    } 
+  });
+}
---
+++ readme.md
@@ -0,0 +1,15 @@
+# ls-modal
+
+basic modal alert
+
+### Change font sizes in css:
+
+:root { <br/>
+    --ls-font-header: 2rem; \
+    --ls-font-body: 1.5rem; \
+}
+
+### Change contents in js
+const modalHeaderText = `Header`; \
+const modalBodyText = `Contents`; \
+const modalBtnText = `Button`;
